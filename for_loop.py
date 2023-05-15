af8a1e6c9aaa43e6fe920c8b271c61845014db95
enriqueMota <abelmota@live.com>
2023-05-15 17:47:11 -0400

    for loops

---
+++ for_loop.py
@@ -0,0 +1,7 @@
+# List to be iterated
+fruits = ["Apple", "Peach", "Pear"]
+
+# Creating a variable for every iteration
+for fruit in fruits:
+    # it is important to stay indented in order to be inside the loop
+    print(fruit)
